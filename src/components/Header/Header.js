683b0c0f07fd76d6672823e0b92534910e9abb7c
Krutagna <kpatel.kp7016@gmail.com>
2023-02-08 18:03:44 +0530

    .

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,18 +3,20 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <div className="header container flex flex--justify-space-between flex--align-center">
-            <div className="header__logo-wrapper">
-                <img className="header__logo"
-                     src={logo}
-                     alt="Rock Paper Scissors"
-                />
-            </div>
-            <div className="header__score-wrapper grid grid--items-center background-white">
-                <p className="header__score-label font-size-100 text-dark-blue letter-spacing-1">Score</p>
-                <p className="header__score font-size-600 text-dark-grey font-weight-bold">0</p>
-            </div>
-        </div>
+        <section className='header-section'>
+            <header className="header container flex flex--justify-space-between flex--align-center">
+                <div className="header__logo-wrapper">
+                    <img className="header__logo"
+                         src={logo}
+                         alt="Rock Paper Scissors"
+                    />
+                </div>
+                <div className="header__score-wrapper grid grid--items-center background-white">
+                    <p className="header__score-label font-size-100 text-dark-blue letter-spacing-1">Score</p>
+                    <p className="header__score font-size-600 text-dark-grey font-weight-bold">0</p>
+                </div>
+            </header>
+        </section>
     )
 }
 
